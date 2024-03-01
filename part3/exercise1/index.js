d21ee2b2eee22953cc21ce0a31f8bd127a226992
mehmetyagci <mehmetyagci53@gmail.com>
2024-03-01 08:26:37 +0300

    Deploying the database backend to production completed.

--- part3/exercise1/index.js
+++ part3/exercise1/index.js
@@ -19,7 +19,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
@@ -46,13 +48,9 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   console.log('index.js -> POST /api/notes')
   const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
 
   const note = new Note({
     content: body.content,
@@ -62,9 +60,11 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
+  console.log('index.js -> GET /api/notes/:id')
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -86,20 +86,18 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
 })
-
 
 app.use(unknownEndpoint)
 // this has to be the last loaded middleware, also all the routes should be registered before this!
