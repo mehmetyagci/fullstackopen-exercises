0130845dd93ffbed7c44217015cc4e7cd2e1704a
mehmetyagci <mehmetyagci53@gmail.com>
2024-03-04 23:00:50 +0300

    Testing Node applications completed

--- part3/exercise1/mongo.js
+++ part3/exercise1/mongo.js
@@ -11,7 +11,7 @@ console.log('password', password)
 
 const url =
   `mongodb+srv://mehmetyagci53:${password}@mycluster.nqz1ts5.mongodb.net/noteApp?retryWrites=true&w=majority`
-console.log('url', url)  
+console.log('url', url)
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -20,14 +20,16 @@ mongoose.connect(url).then(() => {
     content: String,
     important: Boolean,
   })
-  
+
   const Note = mongoose.model('Note', noteSchema)
-  
+
   const note = new Note({
     content: 'HTML is x',
     important: true,
   })
-  
+
+  console.log(note)
+
   /*
   note.save().then(result => {
     console.log('note saved!')
